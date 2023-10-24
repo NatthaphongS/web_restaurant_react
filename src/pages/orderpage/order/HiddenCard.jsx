d27723d9300f298c0d7ce6b3ec9078810cfe7780
NatthaphongS <sinsupharoek@gmail.com>
2023-10-24 14:23:38 +0700

    fix add button

--- src/pages/orderpage/order/HiddenCard.jsx
+++ src/pages/orderpage/order/HiddenCard.jsx
@@ -1,6 +1,6 @@
 export default function HiddenCard() {
   return (
-    <div className="absolute w-full h-full bg-primary bg-opacity-60">
+    <div className="absolute w-full h-full bg-primary bg-opacity-60 z-[10]">
       <div className="w-full h-[180px] flex justify-center items-center">
         <div className="text-whitetext w-fit px-1 aspect-square rounded-full border border-dashed border-mybackground flex justify-center items-center">
           หมดชั่วคราว
--- src/router/RedirectIfOrdering.jsx
+++ src/router/RedirectIfOrdering.jsx
@@ -1,8 +1,6 @@
 import { Navigate } from "react-router-dom";
 
 import useOrder from "../hook/use-order";
-import { useEffect } from "react";
-import axios from "../config/axios";
 import useAuth from "../hook/use-auth";
 
 export default function RedirectIfOrdering({ children }) {

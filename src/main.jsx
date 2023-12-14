358a993f73f1e8945706c8ceeb69d81e3d82d1d7
NatthaphongS <sinsupharoek@gmail.com>
2023-12-14 18:46:47 +0700

    fix react toast2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './index.css';
 import AuthContextProvider from './contexts/AuthContext.jsx';
 

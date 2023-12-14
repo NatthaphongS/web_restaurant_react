e8e3aca07bbf940b96828ba87266217a3ced5533
NatthaphongS <sinsupharoek@gmail.com>
2023-12-14 18:31:25 +0700

    fix react toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "react-toastify/dist/ReactToastify.css";
-import "./index.css";
-import AuthContextProvider from "./contexts/AuthContext.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
+import AuthContextProvider from './contexts/AuthContext.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <AuthContextProvider>
     <App />

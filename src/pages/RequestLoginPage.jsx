b9329c0556c6405033e246eaf4ae14767377e878
NatthaphongS <sinsupharoek@gmail.com>
2023-12-14 23:22:57 +0700

    fix reqlogin

--- src/pages/RequestLoginPage.jsx
+++ src/pages/RequestLoginPage.jsx
@@ -1,27 +1,33 @@
-import { useState } from "react";
-import Button from "../components/Button/Button";
-import UserModal from "../components/usermodal/UserModal";
+import { useState } from 'react';
+import Button from '../components/Button/Button';
+import UserModal from '../components/usermodal/UserModal';
 
 export default function RequestLoginPage() {
   const [isOpenLogin, setIsOpenLogin] = useState(false);
   const [isOpenRegister, setIsOpenRegister] = useState(false);
   return (
     <div
-      style={{ minHeight: "calc(100vh - 80px)" }}
+      style={{ minHeight: 'calc(100vh - 80px)' }}
       className="bg-mybackground flex items-center justify-center flex-col gap-8 py-20"
     >
-      <h3>โปรดเข้าสู่ระบบก่อนสั่งอาหาร</h3>
-      <div className="flex gap-3 h-[400px]">
+      <h3 className="text-[5vw] lg:text-[50px]">
+        โปรดเข้าสู่ระบบก่อนสั่งอาหาร
+      </h3>
+      <div className="flex lg:h-[400px] gap-3 justify-center ">
         <img
           src="/promotion/prom1.png"
           alt="ส่วนลดพิเศษ"
-          className="w-[267px]"
+          className="w-[30%] lg:w-[267px]"
         />
-        <img src="/promotion/prom2.png" alt="ฟรีเบียร์" className="w-[267px]" />
+        <img
+          src="/promotion/prom2.png"
+          alt="ฟรีเบียร์"
+          className="w-[30%] lg:w-[267px]"
+        />
         <img
           src="/promotion/prom3.png"
           alt="ปาร์ตี้ปีใหม่"
-          className="w-[267px]"
+          className="w-[30%] lg:w-[267px]"
         />
       </div>
       <div className="flex gap-10 justify-center items-center">

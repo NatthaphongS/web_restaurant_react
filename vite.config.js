d116f2b2077ca53d8e4f13bcfead255f42ac1ba4
NatthaphongS <sinsupharoek@gmail.com>
2023-12-14 18:49:05 +0700

    fix react toast3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-toastify/dist/ReactToastify.css'],
+    },
   },
 });
